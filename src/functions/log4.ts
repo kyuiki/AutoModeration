5aabc2e474a2d2e30757a137e4aad644a84d4b94
Qky <qkie0915@gmail.com>
2022-02-08 00:27:58 +0700

    chore(log4): Added Webhook Logging

--- src/functions/log4.ts
+++ src/functions/log4.ts
@@ -1,5 +1,13 @@
 import * as day from "dayjs";
 import * as c from "colors/safe";
+import * as Discord from "discord.js";
+require("dotenv-flow").config();
+
+const whlogging = process.env["WEBHOOKLOG"]?.split("/");
+const webhook = new Discord.WebhookClient({
+  id: whlogging[0],
+  token: whlogging[1],
+});
 class Logging {
   /**
    * for Error Message (Red)
@@ -37,15 +45,23 @@ class Logging {
     this.sendConsole("Green", "SUCCESS", ...message);
   }
   private sendConsole(color: string, type: string, ...message) {
+    let log = c.gray(
+      `${c[`bg${color}`]("Log4J")}[${this.date()} ${c[color.toLowerCase()](
+        type
+      )}]:`
+    );
+    let logClean = log.replace(/\[.{2}m/gi, "").replace("Log4J", "");
     console.log(
-      c.gray(
-        `${c[`bg${color}`]("Log4J")}[${this.date()} ${c[color.toLowerCase()](
-          type
-        )}]:`
-      ),
-
+      log,
       ...message.map((x) => (typeof x === "string" ? c.gray(x) : x))
     );
+    if (message.filter((x) => !["string", "number"].includes(typeof x)).length)
+      return;
+    webhook.send({
+      content: logClean + message.join(" "),
+
+      // allowed_mentions: { parse: [] },
+    });
   }
   private date() {
     return c.white(day().format("HH:mm:ss"));
