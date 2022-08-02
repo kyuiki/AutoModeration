e7860f3c2d47802d6e29a8adbee31e6c9b13d6ed
qkiemauln <qkie0915@gmail.com>
2022-08-02 18:03:14 +0700

    feat(event.ts): Add Guild Plugin and MessageMod

--- src/events.ts
+++ src/events.ts
@@ -1,10 +1,21 @@
 import * as Discord from "discord.js";
+import * as fs from "fs";
+
 import commands from "./handlers/commands";
-import memberAuto from "./handlers/memberAutorole";
 import antiphising from "./workers/antiphishing";
+import antibadword from "./workers/antibadword";
 import log4 from "./functions/log4";
 
+const config = require("./json/config.json");
+
 let trackedUsers = new Map();
+
+const plugins = new Discord.Collection(),
+  pluginFiles: any = fs.readdirSync("./src/handlers/guildPlugins").filter((file) => file.endsWith(".ts"));
+for (const file of pluginFiles) {
+  const plugin: any = require(`./handlers/guildPlugins/${file}`);
+  plugins.set(plugin.default.initial.guildID, plugin.default);
+}
 
 export default (client: Discord.Client) => {
   client.on("ready", () => {
@@ -12,10 +23,19 @@ export default (client: Discord.Client) => {
   });
 
   client.on("messageCreate", async (message: Discord.Message) => {
-    // console.log(message.content);
-    message.attachments[0];
+    // Start the Plugin
+    plugins.get(message.guild.id)?.["messageCreate"]?.(client, message);
+
     antiphising(client, message);
+
+    //Find in Config
+    const conf = config.find((a) => a.id == message.guild.id);
+    if (conf) {
+      if (conf.config.antiBadword.isActive) antibadword(client, conf.config.antiBadword, message);
+    }
+    //Run command
     const waiting = await commands(client, message);
+    //Tracker uwu
     if (trackedUsers.has(message.author.id)) trackedUsers.get(message.author.id).chat(message);
 
     if (waiting?.imLogging) {
@@ -30,7 +50,9 @@ export default (client: Discord.Client) => {
     // if (message.author.username == "Qky") return log4.error(message);
   });
   client.on("messageUpdate", async (oldMessage: Discord.Message, message: Discord.Message) => {
-    // console.log(message.content);
+    // Start the Plugin
+    plugins.get(message.guild.id)?.["messageUpdate"]?.(client, oldMessage, message);
+
     const waiting = await commands(client, message);
     if (trackedUsers.has(message.author.id)) trackedUsers.get(message.author.id).chat(message);
 
@@ -46,7 +68,8 @@ export default (client: Discord.Client) => {
     // if (message.author.username == "Qky") return log4.error(message);
   });
   client.on("guildMemberAdd", async (member: Discord.GuildMember) => {
-    memberAuto(client, await member);
+    // Start the Plugin
+    plugins.get(member.guild.id)?.["guildMemberAdd"]?.(client, member);
   });
   client.on("typingStart", (t: Discord.Typing): void => {
     // log4.log("everything good!");
