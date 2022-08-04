e09fa90e50cb76e8c0ac6614da169a9977d05845
qkiemauln <qkie0915@gmail.com>
2022-08-04 07:17:59 +0700

    fix(guildPlugin) : Fix the stupidity

--- src/handlers/guildPlugins/966960365965541406.ts
+++ src/handlers/guildPlugins/966960365965541406.ts
@@ -6,16 +6,17 @@ export default {
   initial: {
     guildID: "966960365965541406"
   },
-  async ["messageCreate"](client: Discord.Client, message: Discord.Message) {
+  async messageCreate(client: Discord.Client, message: Discord.Message) {
     // No Facebook
-    () => {
+    {
       const fbAttachment = message.attachments?.some((x) => /(facebook|fb_reels)/gi.test(x.name));
       const fbRelated = /((?<!no\s))(facebo+k|fb\s)/gi.test(message.cleanContent);
       if (fbAttachment || fbRelated) message.react(message.guild.emojis.cache.get("1001544020050313427"));
-    };
-    // Typing Copier
-    () => {
-      const x = 2;
-    };
-  }
+    }
+  },
+  async messageUpdate(client: Discord.Client, messageOld: Discord.Message, messageNew: Discord.Message) {
+    this.messageCreate(client, messageNew);
+  },
+
+  async guildMemberAdd(client, member) {}
 };
