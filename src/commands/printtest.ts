859ca02ecfdb1d3ba6b938b5741583bb4dce857e
qkiemauln <qkie0915@gmail.com>
2022-08-11 14:31:07 +0700

    feat(printtest): Add debug button and OwnerOnly

--- src/commands/printtest.ts
+++ src/commands/printtest.ts
@@ -3,19 +3,28 @@ import * as Discord from "discord.js";
 const initial = {
   name: "printtest",
   alias: ["prnttst", "test", "pt"],
+  ownerOnly: true,
   needPerms: {
     bool: false,
-    permission: [],
-  },
+    permission: []
+  }
 };
-
+const ptbuttons = ["reportbadword,dummytest", "replyasdev"];
 export default {
   initial,
   execute: (client: Discord.Client, message: Discord.Message, args) => {
-    message.react("☑");
-    message.channel.send("this is a Dummy test");
-    message.reply(
-      `Reply Test \nParsed Arguments : ${JSON.stringify(args.parsed)}`
+    const buttons: any = ptbuttons.map((x) =>
+      new Discord.ButtonBuilder()
+        .setCustomId(x)
+        .setLabel(x)
+        .setStyle(Discord.ButtonStyle.Primary)
+        .setEmoji("🔌" as Discord.EmojiResolvable)
     );
-  },
+    message.react("☑");
+    message.channel.send({
+      content: "this is a Dummy test",
+      components: [new Discord.ActionRowBuilder().addComponents(buttons) as any]
+    });
+    message.reply(`Reply Test \nParsed Arguments : ${JSON.stringify(args.parsed)}`);
+  }
 };
